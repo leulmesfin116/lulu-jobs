dd9db390504bde4c60da9ac19a0d09cf052096dd
leulmesfin116 <leul116hga@gmail.com>
2026-04-06 10:03:09 +0300

    intializing server

--- back-end/server.ts
+++ back-end/server.ts
@@ -4,11 +4,7 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
