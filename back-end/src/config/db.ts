a9c2fe4ac76d887bedb2ada3aa026e2288c80ee8
leulmesfin116 <leul116hga@gmail.com>
2026-04-08 11:07:42 +0300

    initialiazing the database

--- back-end/src/config/db.ts
+++ back-end/src/config/db.ts
@@ -1,15 +1,19 @@
 import { PrismaClient } from "@prisma/client";
 
-export const db = new PrismaClient({
+ const db = new PrismaClient({
   log: process.env.NODE_ENV==="development"?{"query", "error","warn"} :{"error"}
 });
 
 const connectDb=async()=>{
   try{
-    await prisma.$connect()
+    await db.$connect()
     console.log("DB is connected via prisma")
   }catch(error){
     console.log(`DB connection error ${error}`)
+    process.exit(1);
   }
 }
-const disconnectDb=async()=>{}
+const disconnectDb=async()=>{
+  await db.$disconnect()
+}
+export {db,connectDb,disconnectDb};
