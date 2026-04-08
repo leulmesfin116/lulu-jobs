c7c4cfadbb9e81261bf94b7a10c975ee81b9aa33
leulmesfin116 <leul116hga@gmail.com>
2026-04-08 09:17:29 +0300

    initializing the routes

--- back-end/src/Routes/resumeRoute.ts
+++ back-end/src/Routes/resumeRoute.ts
@@ -6,4 +6,4 @@ router.get("/resume", (req, res) => {
   res.send({ message: "resume is being built" });
 });
 
-export default router: 
+export default router;
--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,13 +1,18 @@
 import express from "express";
+import resumeRoute from "./Routes/resumeRoute.ts";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("");
+app.use("/api", resumeRoute);
+
+app.get("/", (req, res) => {
+  res.send("Server is up and running");
+});
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
