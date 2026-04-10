f89b790bb6d7bb00802bc1673a26b9ea20453c16
leulmesfin116 <leul116hga@gmail.com>
2026-04-10 16:52:54 +0300

    authethicating

--- back-end/src/controller/authController.ts
+++ back-end/src/controller/authController.ts
@@ -1,7 +1,17 @@
 import type { Request, Response } from "express";
+import { db } from "../config/db.ts";
 
-const register = async (req: Request, res: Response) => {
-  res.json({ message: "you are registered" });
+type userInput = {
+  name: string;
+  email: string;
+  password: string;
 };
-
+const register = async (req: Request, res: Response) => {
+  const { name, email, password } = req.body;
+  // checking if the user exists
+  const userExists = await db.user.findUnique({ where: { email: email } });
+  if (userExists) {
+    return res.json({ message: "user already exists" });
+  }
+};
 export { register };
--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -10,6 +10,7 @@ disconnectDb();
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // api routes
 app.use("/resume", resumeRoute);
