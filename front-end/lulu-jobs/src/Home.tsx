061cb01b3feef2fa8123d0b85855259722382d64
leulmesfin116 <leul116hga@gmail.com>
2026-04-18 11:07:12 +0300

    home page

--- front-end/lulu-jobs/src/Home.tsx
+++ front-end/lulu-jobs/src/Home.tsx
@@ -5,8 +5,8 @@ function Home() {
         <div></div>
         <div>
           {" "}
-          <h1>
-            Build your <span>Resume</span> with Lulu
+          <h1 className="text-3xl text-center mt-20 font-bold">
+            Build your <span className="text-bold text-green-500">Resume</span> with <span className="underline decoration-green-500 underline-offset-1">Lulu</span>
           </h1>
         </div>
       </div>
