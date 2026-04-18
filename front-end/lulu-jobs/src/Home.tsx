511526be41c2e2786224f2aafdf22252b2e1fa93
leulmesfin116 <leul116hga@gmail.com>
2026-04-18 11:20:10 +0300

    home page

--- front-end/lulu-jobs/src/Home.tsx
+++ front-end/lulu-jobs/src/Home.tsx
@@ -1,14 +1,14 @@
 function Home() {
   return (
     <>
-      <div>
-        <div></div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
         <div>
           {" "}
           <h1 className="text-3xl text-center mt-20 font-bold">
             Build your <span className="text-bold text-green-500">Resume</span> with <span className="underline decoration-green-500 underline-offset-1">Lulu</span>
           </h1>
         </div>
+        <div><h1 className="text-3xl text-center mt-20 font-bold"></h1> picture</div>
       </div>
     </>
   );
